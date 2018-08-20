83d13b00dcda3d0844f314f63db637bb2f08792e
Mateus Schmitz <matteuschmitz@gmail.com>
2018-08-19 22:10:23 -0300

    demo for validations

--- demo.php
+++ demo.php
@@ -10,18 +10,14 @@ require('./vendor/autoload.php');
 define('ROOT_PATH', __DIR__);
 
 $blockchain = new \BlockchainPHP\Blockchain([
-    'name'                  => 'demo',
-    'blocks_dir'            => __DIR__ . '/data/',
-    'indexes_dir'           => __DIR__ . '/data/',
-    'hash_algorithm'        => 'sha256',
-    'hash_cycles' => 2,
-    'block_max_size'        => '5MB',
-    'blocks_file_max_size'  => '500MB',
-    'blocks_file_prefix'    => 'demo',
-    'difficulty'            => 0,
+    'name'                 => 'BlockchainPHP',
+    'version'              => 5,
+    'blocks_dir'           => __DIR__ . '/data/',
+    'block_max_size'       => '5MB',
+    'blocks_file_max_size' => '500MB'
 ]);
 
-$block = (new \BlockchainPHP\Block('This is the content of the block'))
-    ->setMerkleRootHash('hashofmerkleroot');
+$block = new \BlockchainPHP\Block('This is the content of the block');
+$block = new \BlockchainPHP\Block('This is the content of the block', strtotime('now'));
 
 $blockchain->addBlock($block);
