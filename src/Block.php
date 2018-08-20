82e767781b462e959fe025748137c8aafb136f8e
Mateus Schmitz <matteuschmitz@gmail.com>
2018-08-19 22:09:58 -0300

    Blockchain's atributes finished. Block' atributes was begin

--- src/Block.php
+++ src/Block.php
@@ -9,23 +9,33 @@ namespace BlockchainPHP;
 
 class Block
 {
+    private $index;
+
+    private $timestamp;
+
+    private $prevhash;
+
+    private $blockhash;
+
+    private $length;
+
+    private $data;
+
     /**
      * Sets up the data to be saved on block and instance the object
      * 
-     * @param mixed $data            can be string/array/object
-     * @param string $merkleRootHash hash generates by Merkle Root Algorithm
+     * @param mixed   $data      can be string/array/object
+     * @param integer $timestamp blocks's timestamp
      */
-    public function __construct($data = null, $merkleRootHash = null)
-    {}
+    public function __construct($data, $timestamp = null)
+    {
+        $this->data = $data;
+        $this->timestamp = is_null($timestamp) ? strtotime('now') : $timestamp;
+    }
 
     public function validate()
     {}
 
     public function setData($data)
     {}
-
-    public function setMerkleRootHash($merkleRootHash)
-    {
-        return $this;
-    }
 }
--- src/Blockchain.php
+++ src/Blockchain.php
@@ -9,19 +9,83 @@ namespace BlockchainPHP;
 
 class Blockchain
 {
+    const DEFAULT_VERSION = '1';
+
+    const DEFAULT_BLOCK_MAX_SIZE = '10MB';
+
+    const DEFAULT_BLOCKS_FILE_MAX_SIZE = '500MB';
+
+    private $name;
+
+    private $version = self::DEFAULT_VERSION;
+
+    private $blocksDir;
+
+    private $blockMaxSize = self::DEFAULT_BLOCK_MAX_SIZE;
+
+    private $blocksFileMaxSize = self::DEFAULT_BLOCKS_FILE_MAX_SIZE;
+
     /**
      * Sets up all the options to the blockchain and instance the chain
      * 
      * @param array $settings settings to be used
      */
-    public function __construct(array $settings = [])
-    {}
+    public function __construct(array $settings)
+    {
+        $this->setUp($settings);
+    }
+
+    private function validateSetUp()
+    {
+        if (empty($this->name)) {
+            throw new \InvalidArgumentException("Invalid 'name'");
+        }
+
+        if (empty($this->version)) {
+            throw new \InvalidArgumentException("Invalid 'version'");
+        }
+
+        if (!is_writable($this->blocksDir)) {
+            throw new \LogicException("Invalid 'blocks_dir'");
+        }
+
+        if (!preg_match('/^([0-9]){1,4}(KB|MB|GB|TB)$/', $this->blockMaxSize)) {
+            throw new \LengthException("Invalid 'block_max_size'");
+        }
+
+        if (!preg_match('/^([0-9]){1,4}(KB|MB|GB|TB)$/', $this->blocksFileMaxSize)) {
+            throw new \LengthException("Invalid 'blocks_file_max_size'");
+        }
+
+        return true;
+    }
 
     private function setUp(array $settings)
-    {}
+    {
+        if (isset($settings['name']) && !empty($settings['name'])) {
+            $this->name = $settings['name'];
+        }
 
-    private function loadBlockchain()
-    {}
+        if (isset($settings['version']) && !empty($settings['version'])) {
+            $this->version = $settings['version'];
+        }
+
+        if (isset($settings['blocks_dir']) && !empty($settings['blocks_dir'])) {
+            $this->blocksDir = $settings['blocks_dir'];
+        }
+
+        if (isset($settings['block_max_size']) && !empty($settings['block_max_size'])) {
+            $this->blockMaxSize = $settings['block_max_size'];
+        }
+
+        if (isset($settings['blocks_file_max_size']) && !empty($settings['blocks_file_max_size'])) {
+            $this->blocksFileMaxSize = $settings['blocks_file_max_size'];
+        }
+
+        $this->validateSetUp();
+
+        return $this;
+    }
 
     private function getLastPositionBlock()
     {}
@@ -30,6 +94,9 @@ class Blockchain
     {}
 
     public function addBlock(Block $block)
+    {}
+
+    public function getBlocks()
     {}
 
     public function getLastBlock()
